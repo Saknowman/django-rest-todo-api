27b54baf6fb0911d8d93cb13d6e4487afb5d7f03
saknowman <snowman.sucking@gmail.com>
2019-11-04 23:06:37 +0900

    Fix requirements django-framework to djangoframework.

--- setup.py
+++ setup.py
@@ -31,5 +31,5 @@ setup(
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],
-    install_requires=['django-restframework', 'django-filter']
+    install_requires=['djangorestframework', 'django-filter']
 )
